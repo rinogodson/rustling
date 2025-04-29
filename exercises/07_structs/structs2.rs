0e326711caad9abe0881ebd346baf434a1bd6409
rinogodson <rinogodson@gmail.com>
2025-04-29 22:33:14 +0530

    finished structs2.rs

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -35,6 +35,9 @@ mod tests {
 
         // TODO: Create your own order using the update syntax and template above!
         // let your_order =
+        let mut your_order = create_order_template();
+        your_order.count = 1; 
+        your_order.name = "Hacker in Rust".to_string();
 
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
